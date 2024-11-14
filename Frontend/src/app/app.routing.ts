4394368b913b67d69c8245783be646d427f9800a
Gorka <gorkarual90@gmail.com>
2024-11-14 13:07:49 +0100

    registro de datos terminado

--- Frontend/src/app/app.routing.ts
+++ Frontend/src/app/app.routing.ts
@@ -16,6 +16,10 @@ import { MostrarAtributosComponent } from './mostrar-atributos/mostrar-atributos
 import { MapGeoserverWFSComponent } from './map-geoserver-wfs/map-geoserver-wfs.component';
 import { MapGeoserverWMSComponent } from './map-geoserver-wms/map-geoserver-wms.component';
 import { MapComponent } from "./map/map.component";
+import { Datos_identificacionComponent } from "./datos_identificacion/datos_identificacion.component";
+import { Datos_biometricosComponent } from "./datos_biometricos/datos_biometricos.component";
+import { Datos_sanitariosComponent } from "./datos_sanitarios/datos_sanitarios.component";
+import { Datos_actuacionesComponent } from "./datos_actuaciones/datos_actuaciones.component";
 //rutas de la página web
 const appRoutes: Routes = [
 {path:'inicio',component: InicioComponent},                 //ruta del navegador a acceder, componente a cargar
@@ -26,10 +30,14 @@ const appRoutes: Routes = [
 {path:'tareas',component: TareasComponent},                 //ruta del navegador a acceder, componente a cargar
 {path:'arboles/:id/:altura/:diametro/:nombre',component: ArbolesComponent},                 //ruta del navegador a acceder, componente a cargar
 {path:'lista', component: MostrarAtributosComponent },
-{path:'datos',component: DatosComponent},                 //ruta del navegador a acceder, componente a cargar
-{path:'ayuda',component: AyudaComponent},                 //ruta del navegador a acceder, componente a cargar
-{path:'perfil',component: PerfilComponent},                 //ruta del navegador a acceder, componente a cargar
-{path:'acceso',component: AccesoComponent},                 //ruta del navegador a acceder, componente a cargar
+{path:'datos', component: DatosComponent},                 //ruta del navegador a acceder, componente a cargar
+{path:'identificacion', component: Datos_identificacionComponent},  //ruta para llegar a datos_identificación
+{path:'biometria', component: Datos_biometricosComponent},  //ruta para llegar a datos_biometricos
+{path:'salud', component: Datos_sanitariosComponent},      //ruta para llegar a datos_sanitarios
+{path:'actuaciones', component: Datos_actuacionesComponent},  //ruta para llegar a datos_actuaciones
+{path:'ayuda', component: AyudaComponent},                 //ruta del navegador a acceder, componente a cargar
+{path:'perfil', component: PerfilComponent},                 //ruta del navegador a acceder, componente a cargar
+{path:'acceso', component: AccesoComponent},                 //ruta del navegador a acceder, componente a cargar
 {path:'arboles_tarjetas',component: Arboles_tarjetasComponent},                 //ruta del navegador a acceder, componente a cargar
 {path:'**',component: InicioComponent},               //si da un error, siempre nos dirige a la página principal
 
