c26db3e6c319ee7f3e901b80198a3c74f2f51615
holmedo <holmedo@birt.eus>
2024-11-17 20:50:00 +0100

    zuhaitzguneappFrontend04: recojo en locationData y dasometricData

---
+++ zuhaitzguneappFrontend04/src/app/formularioNew/datos_menu/datos_menu.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router'; // Importa esto
+import { NgModule } from '@angular/core';
+/*
+@Component({
+  selector: 'app-datos_menu',
+  templateUrl: './datos_menu.component.html',
+  styleUrl: './datos_menu.component.css'
+})
+  */
+
+
+@NgModule({
+  exports: [RouterModule]
+})
+export class Datos_menufooterComponent {
+
+}
