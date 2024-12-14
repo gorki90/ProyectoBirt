6f39185ddd6112176078bf844b979e1ab31acf1e
Gorka <gorkarual90@gmail.com>
2024-12-14 16:31:23 +0100

    Temas implementados

--- verison241211/zuhaitzguneappFrontend9/src/app/app.component.ts
+++ verison241211/zuhaitzguneappFrontend9/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
 import { WfsService } from './servicio/wfs.service';
+import { ThemeService } from './servicio/themeService';
 //import { Datos_menuComponent, } from './formularioNew/datos_menu/datos_menu.component';
 //import { Datos_menufooterComponent } from './formularioNew/datos_menufooter/datos_menufooter.component';
 //import { MostrarAtributosComponent } from './mostrar-atributos/mostrar-atributos.component';
@@ -25,17 +26,24 @@ export class AppComponent implements OnInit{
   title = 'Zuhaitzguneapp';
   nombre: any|string;
   foto:string="";
+  currentTheme: 'dark' | 'light';
 
 
-  constructor(private router: Router,private _http:WfsService){
+  constructor(private router: Router,private _http:WfsService, private themeService: ThemeService){
     console.log("Componente principal generado!!!")
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
         this.getDatosUser();
       }
     });
+    this.currentTheme = localStorage.getItem('theme') as 'dark' | 'light' || 'light';
   }
   ngOnInit(): void {
+    if (this.currentTheme === 'dark') {
+      this.themeService.enableDarkTheme();
+    } else {
+      this.themeService.enableLightTheme();
+    }
   }
   // metodo para destruir y volver a cargar el componente asociado
   forzarRecarga() {
@@ -62,6 +70,18 @@ export class AppComponent implements OnInit{
       }
     })
    }
+  }
+
+  toggleTheme(): void {
+    
+    if (this.currentTheme === 'light') {
+      this.currentTheme = 'dark';
+      this.themeService.enableDarkTheme();
+    } else {
+      this.currentTheme = 'light';
+      this.themeService.enableLightTheme();
+    }
+    localStorage.setItem('theme', this.currentTheme);
   }
 
 }
